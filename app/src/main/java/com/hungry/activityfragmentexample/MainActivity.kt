900b51d4807e0783dd800adf1a1539ee784fb0bc
Dian Permata Kusuma <diankusuma664@gmail.com>
2023-09-20 15:28:31 +0700

    finish feature fragment manager

--- app/src/main/java/com/hungry/activityfragmentexample/MainActivity.kt
+++ app/src/main/java/com/hungry/activityfragmentexample/MainActivity.kt
@@ -2,10 +2,44 @@ package com.hungry.activityfragmentexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.hungry.activityfragmentexample.databinding.ActivityMain2Binding
+import com.hungry.activityfragmentexample.presentation.fragmentone.FragmentOne
+import com.hungry.activityfragmentexample.presentation.fragmentthree.FragmentThree
+import com.hungry.activityfragmentexample.presentation.fragmenttwo.FragmentTwo
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentOne: FragmentOne by lazy { FragmentOne() }
+    private val fragmentTwo: FragmentTwo by lazy { FragmentTwo() }
+    private val fragmentThree: FragmentThree by lazy { FragmentThree() }
+
+    private val binding: ActivityMain2Binding by lazy {
+        ActivityMain2Binding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setClickButtonNavigate()
+        replaceFragment(fragmentOne)
+    }
+
+    private fun setClickButtonNavigate() {
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(fragmentOne)
+        }
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(fragmentTwo)
+        }
+        binding.btnFragment3.setOnClickListener {
+            replaceFragment(fragmentThree)
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.flContainer.id, fragment)
+            .addToBackStack(fragment.javaClass.simpleName)
+            .commit()
     }
 }
